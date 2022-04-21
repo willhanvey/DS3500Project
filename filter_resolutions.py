468dd93c4792c242979aa6b68128fb084e88d631
Alejandro Bermejo <alejandro@bermejo.app>
2022-04-21 18:14:32 -0400

    Graph data created

--- filter_resolutions.py
+++ filter_resolutions.py
@@ -25,13 +25,15 @@ bees = list(filter(q.match, files_in_dir))
 if len(bees)>0:
     print(bees)
 """
-with open('resolutions.csv') as inp, open('filtered_resolutions.csv', 'w') as out:
+with open('UN DATA.csv') as inp, open('filtered_resolutions.csv', 'w') as out:
     reader = csv.DictReader(inp)
     writer = csv.DictWriter(out, reader.fieldnames)
     writer.writeheader()
     for row in tqdm(reader):
-        e = row["resolution"].split("/")
-        if e[0]=="R" and len(e)==3:
+        e = row["Resolution"].split("/")
+        if len(e)==3:
+            e[2] = e[2].split("(")[0]
+            print(e)
             r = re.compile("A[\.\w-]*_{}[\.\w-]*{}.*\.txt".format(e[1], e[2]))
             available = list(filter(r.match, files_in_dir))
             if len(available) > 0:
